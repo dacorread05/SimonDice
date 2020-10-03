6528e145fa7c9f37473726db52f019a0a14287fe
DacorreaD <dacorrea91@ucatolica.edu.co>
2020-10-02 21:53:09 -0500

    Se genero una secuencia de numeros aleatorios entre 0 y 3 y se guardo en un vector

--- src/index.js
+++ src/index.js
@@ -7,12 +7,16 @@ const btnEmpezar = document.getElementById('btnEmpezar');
 class Juego {
     constructor() {
         this.inicializar();
+        this.generarSecuencia();
     }
     inicializar() {
         btnEmpezar.classList.add('hide')
     }
+    generarSecuencia(){
+        this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
+    }
 }
 
 function empezarJuego() {
-    var juego = new Juego()
+     window.juego = new Juego()
 }
